236890b08d1bbf77cc6359e125f2d5884e6db2bd
azxca1731 <azxca1731@gmail.com>
2019-03-09 00:55:02 +0900

    mock up Dropdown

--- src/components/UserDropdown/UserDropdown.jsx
+++ src/components/UserDropdown/UserDropdown.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
-
-import { faHome } from "@fortawesome/free-solid-svg-icons";
+import { DropdownItem } from "reactstrap";
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const UserDropdownLeftAlign = styled.div`
@@ -10,17 +10,46 @@ const UserDropdownLeftAlign = styled.div`
 `;
 
 const Dropdown = styled.div`
+	display: ${props => (props.isOpen ? "block" : "none")};
 	position: absolute;
-	top: 0px;
+	top: 30px;
+	right: 12px;
+	border: 1px solid rgba(0, 0, 0, 0.15);
+	box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+	border-radius: 0.25rem;
 `;
 
-const UserDropdown = () => {
-	return (
-		<UserDropdownLeftAlign className="mt-4 d-none d-md-flex">
-			UserName <FontAwesomeIcon icon={faHome} />
-			<Dropdown />
-		</UserDropdownLeftAlign>
-	);
-};
+class UserDropdown extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			isOpen: false
+		};
+	}
+
+	render() {
+		const { isOpen } = this.state;
+		return (
+			<UserDropdownLeftAlign className="mt-4 d-none d-md-flex">
+				UserName
+				<FontAwesomeIcon
+					icon={faChevronDown}
+					className="ml-3 mr-3"
+					style={{ position: "relative", top: "5px" }}
+					onClick={() => {
+						this.setState({ isOpen: !isOpen });
+					}}
+				/>
+				<Dropdown isOpen={isOpen}>
+					<DropdownItem header>Header</DropdownItem>
+					<DropdownItem disabled>Action</DropdownItem>
+					<DropdownItem>Another Action</DropdownItem>
+					<DropdownItem divider />
+					<DropdownItem>Another Action</DropdownItem>
+				</Dropdown>
+			</UserDropdownLeftAlign>
+		);
+	}
+}
 
 export default UserDropdown;
