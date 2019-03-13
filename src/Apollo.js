578147939c302a92723052304bafaee80085778e
azxca1731 <azxca1731@gmail.com>
2019-03-13 13:55:37 +0900

    netlify setting

--- src/Apollo.js
+++ src/Apollo.js
@@ -1,7 +1,9 @@
 import ApolloClient from "apollo-boost";
 
 const client = new ApolloClient({
-	uri: "http://localhost:4000"
+	uri: process.env["REMODY_SERVER"]
+		? process.env["REMODY_SERVER"]
+		: "http://localhost:4000"
 });
 
 export default client;
