dc9f5b10cf54d08f2069b1de25c97293d4d38d3e
azxca1731 <azxca1731@gmail.com>
2019-03-15 12:47:30 +0900

    add AuthLink

--- src/Apollo.js
+++ src/Apollo.js
@@ -1,10 +1,24 @@
 import { ApolloClient } from "apollo-client";
 import { createUploadLink } from "apollo-upload-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
+import { setContext } from "apollo-link-context";
+
+const uploadLink = createUploadLink({ uri: "http://localhost:4000" });
+
+const authLink = setContext((_, { headers }) => {
+	const token = localStorage.getItem("token");
+	console.log(token);
+	return {
+		headers: {
+			...headers,
+			authorization: token ? `Bearer ${token}` : ""
+		}
+	};
+});
 
 const client = new ApolloClient({
 	cache: new InMemoryCache(),
-	link: createUploadLink({ uri: "http://localhost:4000" })
+	link: authLink.concat(uploadLink)
 });
 
 export default client;
