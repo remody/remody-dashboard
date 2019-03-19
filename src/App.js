8a50f723f02e3ab25503ccb3d58f19c125ccc661
azxca1731 <azxca1731@gmail.com>
2019-03-20 00:36:18 +0900

    themeprovider추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
 import { ApolloProvider } from "react-apollo";
+import { ThemeProvider } from "styled-components";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import client from "./Apollo";
+import theme from "./Theme";
 
 import Home from "./pages/home";
 import Data from "./pages/data";
@@ -15,14 +17,16 @@ class App extends Component {
 	render() {
 		return (
 			<ApolloProvider client={client}>
-				<Router>
-					<Layout>
-						<Route exact path="/" component={Home} />
-						<Route path="/data" component={Data} />
-						<Route path="/interprete" component={Interprete} />
-						<Route path="/analyze" component={Analyze} />
-					</Layout>
-				</Router>
+				<ThemeProvider theme={theme}>
+					<Router>
+						<Layout>
+							<Route exact path="/" component={Home} />
+							<Route path="/data" component={Data} />
+							<Route path="/interprete" component={Interprete} />
+							<Route path="/analyze" component={Analyze} />
+						</Layout>
+					</Router>
+				</ThemeProvider>
 			</ApolloProvider>
 		);
 	}
