0d6a39fc5abd682d88a152dbafa45fb61a855b01
azxca1731 <azxca1731@gmail.com>
2019-03-19 22:58:30 +0900

    필요없는 데이터 삭제

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -14,11 +14,6 @@ export const LOGIN = gql`
 	mutation login($email: String!, $password: String!) {
 		login(data: { email: $email, password: $password }) {
 			token
-			user {
-				id
-				name
-				email
-			}
 		}
 	}
 `;
@@ -27,11 +22,6 @@ export const CREATE_USER = gql`
 	mutation createUser($name: String!, $email: String!, $password: String!) {
 		createUser(data: { name: $name, email: $email, password: $password }) {
 			token
-			user {
-				id
-				name
-				email
-			}
 		}
 	}
 `;
