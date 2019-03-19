0e28690075ac7d8c7ab9cc473eb17fa8db3a89f1
azxca1731 <azxca1731@gmail.com>
2019-03-19 22:47:02 +0900

    react hook으로 변경 및 아이콘 수정

--- src/components/SigninModal/SigninModal.jsx
+++ src/components/SigninModal/SigninModal.jsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
-import { faMailBulk, faKey } from "@fortawesome/free-solid-svg-icons";
+import {
+	faMailBulk,
+	faKey,
+	faAddressCard
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Modal, ModalBody, Button, ModalHeader, Input } from "reactstrap";
 import { Mutation } from "react-apollo";
@@ -32,191 +36,131 @@ const LoadingCenterDiv = styled.div`
 	align-items: center;
 `;
 
-class SigninModal extends React.Component {
-	constructor(props) {
-		super(props);
+const SigninModal = props => {
+	const [email, handleEmail] = useState("");
+	const [password, handlePassword] = useState("");
+	const [confirmPassword, handleConfirmpassword] = useState("");
+	const [name, handleName] = useState("");
 
-		this.state = {
-			emailInput: "",
-			passwordInput: "",
-			secondpasswordInput: "",
-			nameInput: ""
-		};
-	}
-
-	shouldComponentUpdate(nextProps, nextStates) {
-		if (nextProps.isOpen !== this.props.isOpen) {
-			return true;
-		} else if (nextStates !== this.state) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-
-	handleEmailInput = event => {
-		this.setState({
-			emailInput: event.target.value
-		});
-	};
-
-	handlePasswordInput = event => {
-		this.setState({
-			passwordInput: event.target.value
-		});
-	};
-
-	handleSecondPasswordInput = event => {
-		this.setState({
-			secondpasswordInput: event.target.value
-		});
-	};
-
-	handleNameInput = event => {
-		this.setState({
-			nameInput: event.target.value
-		});
-	};
-
-	render() {
-		return (
-			<Mutation mutation={LOGIN}>
-				{(login, { loading, error, data }) => {
-					if (data && data.login && data.login.token) {
-						localStorage.setItem("token", data.login.token);
-						this.props.handleLogin(true);
-						return null;
-					}
-					return (
-						<Modal
-							isOpen={this.props.isOpen}
-							toggle={() => this.props.handleSignInModal(false)}
-							style={{ position: "relative", top: "10%" }}
+	return (
+		<Mutation mutation={LOGIN}>
+			{(login, { loading, error, data }) => {
+				if (data && data.login && data.login.token) {
+					localStorage.setItem("token", data.login.token);
+					props.handleLogin(true);
+					return null;
+				}
+				return (
+					<Modal
+						isOpen={props.isOpen}
+						toggle={() => props.handleSignInModal(false)}
+						style={{ position: "relative", top: "10%" }}
+					>
+						<ModalHeader
+							toggle={() => props.handleSignInModal(false)}
 						>
-							<ModalHeader
-								toggle={() =>
-									this.props.handleSignInModal(false)
-								}
+							Signin
+						</ModalHeader>
+						{loading ? (
+							<LoadingCenterDiv>
+								<ReactLoading type="bubbles" color="pink" />
+							</LoadingCenterDiv>
+						) : (
+							<ModalBody
+								style={{
+									padding: "20px 35px",
+									textAlign: "center"
+								}}
 							>
-								Signin
-							</ModalHeader>
-							{loading ? (
-								<LoadingCenterDiv>
-									<ReactLoading type="bubbles" color="pink" />
-								</LoadingCenterDiv>
-							) : (
-								<ModalBody
-									style={{
-										padding: "20px 35px",
-										textAlign: "center"
-									}}
-								>
-									<div style={{ textAlign: "left" }}>
-										{error ? (
-											<ErrorHeader>error</ErrorHeader>
-										) : (
-											<div />
-										)}
-										<label>
-											<FontAwesomeIcon
-												icon={faMailBulk}
-												className="mr-1"
-											/>
-											E-mail
-										</label>
-										<br />
-										<Input
-											type="email"
-											onChange={this.handleEmailInput}
-											value={this.state.emailInput}
+								<div style={{ textAlign: "left" }}>
+									{error ? (
+										<ErrorHeader>error</ErrorHeader>
+									) : (
+										<div />
+									)}
+									<label>
+										<FontAwesomeIcon
+											icon={faMailBulk}
+											className="mr-1"
 										/>
-									</div>
+										E-mail
+									</label>
 									<br />
-									<div style={{ textAlign: "left" }}>
-										<label>
-											<FontAwesomeIcon
-												icon={faKey}
-												className="mr-1"
-											/>
-											Password
-										</label>
-										<br />
-										<Input
-											type="password"
-											onChange={
-												this.handleSecondPasswordInput
-											}
-											value={
-												this.state.secondpasswordInput
-											}
-										/>
-									</div>
-									<br />
-									<div style={{ textAlign: "left" }}>
-										<label>
-											<FontAwesomeIcon
-												icon={faKey}
-												className="mr-1"
-											/>
-											Password
-										</label>
-										<br />
-										<Input
-											type="password"
-											onChange={this.handlePasswordInput}
-											value={this.state.passwordInput}
-										/>
-									</div>
-									<br />
-									<div style={{ textAlign: "left" }}>
-										<label>
-											<FontAwesomeIcon
-												icon={faKey}
-												className="mr-1"
-											/>
-											Password
-										</label>
-										<br />
-										<Input
-											type="password"
-											onChange={
-												this.handleSecondPasswordInput
-											}
-											value={
-												this.state.secondpasswordInput
-											}
-										/>
-									</div>
-									<br />
-									<Button
-										onClick={() => {
-											login({
-												variables: {
-													email: this.state
-														.emailInput,
-													password: this.state
-														.passwordInput
-												}
-											});
-										}}
-									>
-										Login
-									</Button>
-								</ModalBody>
-							)}
-							<ModalFooter style={{ textAlign: "center" }}>
-								Forgot your password?
+									<Input
+										type="email"
+										onChange={e =>
+											handleEmail(e.target.value)
+										}
+										value={email}
+									/>
+								</div>
 								<br />
-								or
+								<div style={{ textAlign: "left" }}>
+									<label>
+										<FontAwesomeIcon
+											icon={faAddressCard}
+											className="mr-1"
+										/>
+										Name
+									</label>
+									<br />
+									<Input
+										onChange={e =>
+											handleName(e.target.value)
+										}
+										value={name}
+									/>
+								</div>
 								<br />
-								Create new Email?
-							</ModalFooter>
-						</Modal>
-					);
-				}}
-			</Mutation>
-		);
-	}
-}
+								<div style={{ textAlign: "left" }}>
+									<label>
+										<FontAwesomeIcon
+											icon={faKey}
+											className="mr-1"
+										/>
+										Password
+									</label>
+									<br />
+									<Input
+										type="password"
+										onChange={e =>
+											handlePassword(e.target.value)
+										}
+										value={password}
+									/>
+								</div>
+								<br />
+								<div style={{ textAlign: "left" }}>
+									<label>
+										<FontAwesomeIcon
+											icon={faKey}
+											className="mr-1"
+										/>
+										Password Confirm
+									</label>
+									<br />
+									<Input
+										type="password"
+										onChange={e =>
+											handleConfirmpassword(
+												e.target.value
+											)
+										}
+										value={confirmPassword}
+									/>
+								</div>
+								<br />
+								<Button onClick={() => {}}>Login</Button>
+							</ModalBody>
+						)}
+						<ModalFooter style={{ textAlign: "center" }}>
+							Welcome Remody!
+						</ModalFooter>
+					</Modal>
+				);
+			}}
+		</Mutation>
+	);
+};
 
 export default SigninModal;
