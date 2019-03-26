695f06433622fb677a912a3ec30cf4a4dc986ff7
azxca1731 <azxca1731@gmail.com>
2019-03-26 19:14:06 +0900

    remody server url setting

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
 
+console.log(process.env["REMODY_SERVER_URL"]);
 ReactDOM.render(<App />, document.getElementById("root"));
