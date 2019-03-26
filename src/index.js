30fad2af5347b30dd4fd6071ec57f4989fd56ed8
azxca1731 <azxca1731@gmail.com>
2019-03-26 19:16:39 +0900

    link setting

--- src/index.js
+++ src/index.js
@@ -3,5 +3,4 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
 
-console.log(process.env["REMODY_SERVER_URL"]);
 ReactDOM.render(<App />, document.getElementById("root"));
