5962ec473755689035d97a3a0a3d2359665797e6
azxca1731 <azxca1731@gmail.com>
2019-04-02 23:40:50 +0900

    1차 데이트 카드 목업 완료

--- src/components/DataCard/DataCard.jsx
+++ src/components/DataCard/DataCard.jsx
@@ -1,13 +1,36 @@
 import React from "react";
-// import {} from "reactstrap";
-// import styled from "styled-components";
+import styled from "styled-components";
 
-const DataCard = ({ name, rowCount }) => {
+const CardBody = styled.div`
+    background-color: ${props => props.theme.secondaryBackgroundColor};
+    width: 100%;
+    padding: 25px;
+    margin-bottom: 25px;
+    display: flex;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+`;
+
+const CardLeft = styled.div`
+    width: 60%;
+`;
+
+const CardRight = styled.div`
+    width: 40%;
+    text-align: right;
+    padding-top: 20px;
+`;
+
+const DataCard = ({ name, rowCount, user: { name: userName } }) => {
     return (
-        <div>
-            {name}
-            {rowCount}
-        </div>
+        <CardBody>
+            <CardLeft>
+                <h3>{name}</h3>
+            </CardLeft>
+            <CardRight>
+                <div>데이터 수: {rowCount}</div>
+                <div>소유자: {userName}</div>
+            </CardRight>
+        </CardBody>
     );
 };
 export default DataCard;
--- src/graphql/index.js
+++ src/graphql/index.js
@@ -66,6 +66,9 @@ export const USER_SCHEMAS = gql`
             id
             name
             rowCount
+            user {
+                name
+            }
         }
     }
 `;
