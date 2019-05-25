0f6e14289cf74585bd85c155ea7b1be08c0333f7
kmlee95 <kmlee95@naver.com>
2019-05-25 19:22:57 +0900

    home(USER Overview, Device)

---
+++ src/containers/HomeContainer/HomeContainer.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Container, Row, Col } from "shards-react";
+import PageTitle from "../../components/CommonHome/PageTitle";
+import UsersOverview from "../../components/CommonHome/UsersOverview";
+import UsersByDevice from "../../components/CommonHome/UsersByDevice";
+
+const HomeContainer = () => (
+    <Container fluid className="main-content-container px-4">
+        <Row noGutters className="page-header py-4">
+            <PageTitle
+                title="Unstructured document conversion"
+                subtitle="Dashboard"
+                className="text-sm-left mb-3"
+            />
+        </Row>
+        <Row>
+            {/* Users Overview */}
+            <Col lg="8" md="12" sm="12" className="mb-4">
+                <UsersOverview />
+            </Col>
+
+            {/* Users by Device */}
+            <Col lg="4" md="6" sm="12" className="mb-4">
+                <UsersByDevice />
+            </Col>
+        </Row>
+    </Container>
+);
+export default HomeContainer;
